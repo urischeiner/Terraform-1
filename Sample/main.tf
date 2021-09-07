bcedcc1f27cf9835742c0d559003e3a3561d80e9
urischeiner <59268818+urischeiner@users.noreply.github.com>
2021-09-07 15:57:59 -0700

    Update main.tf

--- Sample/main.tf
+++ Sample/main.tf
@@ -1,16 +1,48 @@
-resource "null_resource" "delay" {
-  provisioner "local-exec" {
-    command = var.sleep
-    interpreter = ["/bin/sleep"]
+provider "aws" {
+  region     = var.region
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+resource "aws_security_group" "sg-test" {
+  description = "dev_test security group allows ssh for everyone"
+  name        = "allow-ssh-${var.tag_name}"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-output "version" {
-  value = var.tfversion
+resource "aws_instance" "jarService" {
+  ami = "ami-040ba316fc6ab09df"
+
+  instance_type   = "t2.micro"
+  key_name        = "cdp-test"
+  security_groups = [aws_security_group.sg-test.name]
+ 
 }
-output "color" {
-  value = var.tfcolor
+
+output "security_group" {
+  value = [aws_security_group.sg-test.id]
 }
-output "foo" {
-  value = var.tffoo
+
+output "region" {
+  value = var.region
+}
+
+output "instanceTags" {
+  value = aws_instance.jarService.tags
+}
+
+output "jarServiceTags" {
+  value = var.tag_name
+}
+
+output "dns" {
+  value = aws_instance.jarService.public_dns
+}
+output "vpc" {
+  value = aws_instance.jarService.vpc_security_group_ids
 }
