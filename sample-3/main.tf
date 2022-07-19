9775c4104390d3653f1580da79a81536d0ba7d4b
urischeiner <59268818+urischeiner@users.noreply.github.com>
2022-07-19 16:19:35 -0700

    Update main.tf

--- sample-3/main.tf
+++ sample-3/main.tf
@@ -1,4 +1,10 @@
 module "UriModule1" {
   source = "api.env0.com/bd63324c-0beb-44d4-85ec-a967ecc4e21b/UriModule1/uricompany"
   version = "1.0.0"
+  bucket_name = "uri-test-jul-19-2022"
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
 }
