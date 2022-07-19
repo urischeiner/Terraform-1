2f1c28398ac4ec117547db8ac16e97911094a7d6
urischeiner <59268818+urischeiner@users.noreply.github.com>
2022-07-19 16:42:21 -0700

    Update main.tf

--- sample-3/main.tf
+++ sample-3/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 module "UriModule1" {
   source = "api.env0.com/bd63324c-0beb-44d4-85ec-a967ecc4e21b/UriModule1/uricompany"
   version = "1.0.0"
-  bucket_name = "uri-test-jul-19-2022"
+  bucket_name = var.mybucket
 
   tags = {
     Terraform   = "true"
