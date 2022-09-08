9efce9f5ce83df5185f7eda91e78a097e26d591a
urischeiner <59268818+urischeiner@users.noreply.github.com>
2022-09-08 14:11:24 -0700

    Update output.tf

--- sample-2/output.tf
+++ sample-2/output.tf
@@ -1,6 +1,6 @@
 output "instance_id" {
   description = "ID of the EC2 instance"
-  value       = aws_instance.app_server.id
+  value       = aws_instance.app_server[0].id
 }
 
 output "instance_public_ip" {
