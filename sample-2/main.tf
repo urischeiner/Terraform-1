29df934b424a309949578f68e7d6af55b945ca60
urischeiner <59268818+urischeiner@users.noreply.github.com>
2022-09-23 11:36:56 -0700

    Update main.tf

--- sample-2/main.tf
+++ sample-2/main.tf
@@ -24,7 +24,7 @@ resource "aws_instance" "app_server" {
     # The AMI ID must refer to an AMI that contains an operating system
     # for the `x86_64` architecture.
     precondition {
-      condition     = data.aws_ami.app_server.architecture == "x86_64"
+      condition     = aws_ami.app_server.architecture == "x86_64"
       error_message = "The selected AMI must be for the x86_64 architecture."
     }
   }
