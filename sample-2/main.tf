8f68f9f2afb94f6f9249430026e95eef3c257c7b
urischeiner <59268818+urischeiner@users.noreply.github.com>
2022-09-23 11:32:03 -0700

    Update main.tf

--- sample-2/main.tf
+++ sample-2/main.tf
@@ -19,4 +19,13 @@ resource "aws_instance" "app_server" {
   tags = {
     Name = "ExampleAppServerInstance-Uri-7"
   }
+  
+  lifecycle {
+    # The AMI ID must refer to an AMI that contains an operating system
+    # for the `x86_64` architecture.
+    precondition {
+      condition     = data.aws_ami.example.architecture == "x86_64"
+      error_message = "The selected AMI must be for the x86_64 architecture."
+    }
+  }
 }
