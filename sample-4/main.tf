2781789baaa4d02eabb615e2d370d649cc7fbc62
urischeiner <59268818+urischeiner@users.noreply.github.com>
2022-10-05 14:00:21 -0700

    Update main.tf

--- sample-4/main.tf
+++ sample-4/main.tf
@@ -4,16 +4,7 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 4.16"
     }
-  }
- backend "local" {
-    path = "relative/path/to/terraform.tfstate"
- }
- 
-// backend "s3" {
-//    bucket     = "tf-uri-bucket"
-//    key        = "us-west-2/drone.tfstate"
-//    region     = "us-west-2"
-//  }  
+
   required_version = ">= 1.2.0"
 }
 
