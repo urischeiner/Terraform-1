65604f65f7eb13974e73af7178c606b26512db5e
urischeiner <59268818+urischeiner@users.noreply.github.com>
2022-10-05 14:03:49 -0700

    Update main.tf

--- sample-4/main.tf
+++ sample-4/main.tf
@@ -4,19 +4,37 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 4.16"
     }
+  }
 
   required_version = ">= 1.2.0"
 }
 
 provider "aws" {
-  region  = "us-west-2" 
+  region  = "us-west-2"
+}
+
+data "aws_ami" "app_server" {
+  filter {
+    name = "image-id"
+    values = ["ami-830c94e3"]
+  }
+
+  owners = ["099720109477"] # Canonical
 }
 
 resource "aws_instance" "app_server" {
   ami           = "ami-830c94e3"
-  instance_type = "${var.type}"
-
+  instance_type = "t2.small"
   tags = {
-    Name = "ExampleAppServerInstance-Uri-10"
+    Name = "ExampleAppServerInstance-Uri-7"
+  }
+  
+  lifecycle {
+    # The AMI ID must refer to an AMI that contains an operating system
+    # for the `x86_64` architecture.
+    precondition {
+      condition     = data.aws_ami.app_server.architecture == "x86_64"
+      error_message = "The selected AMI must be for the x86_64 architecture."
+    }
   }
 }
