4903ee784ed3c5bcfa7c399ad188f31299937cc7
urischeiner <59268818+urischeiner@users.noreply.github.com>
2022-10-19 15:57:47 -0700

    Update main.tf

--- sample-4/main.tf
+++ sample-4/main.tf
@@ -25,7 +25,6 @@ data "aws_ami" "app_server" {
 resource "aws_instance" "app_server" {
   ami           = "ami-830c94e3"
   instance_type = var.type
-  subnet_id = "subnet-0d4be14d5f95eec6d"
   tags = {
     Name = "ExampleAppServerInstance-Uri-spacelift-5"
   }
