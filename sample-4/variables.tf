328c83a7e9aab5dee526607e7817be92f7a499e5
urischeiner <59268818+urischeiner@users.noreply.github.com>
2022-11-01 11:52:03 -0700

    Update variables.tf

--- sample-4/variables.tf
+++ sample-4/variables.tf
@@ -1,5 +1,5 @@
-variable "AWS_ACCESS_KEY_ID" {}
-variable "AWS_SECRET_ACCESS_KEY" {}
+# variable "AWS_ACCESS_KEY_ID" {}
+# variable "AWS_SECRET_ACCESS_KEY" {}
 variable "type" {
   type        = string
   description = "AWS Region required by Terraform AWS Provider"
