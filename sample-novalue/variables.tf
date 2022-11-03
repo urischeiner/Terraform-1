5e60b9e2a2a037806bbe636a5b5fc37bb0cd32b2
urischeiner <59268818+urischeiner@users.noreply.github.com>
2022-11-03 07:42:30 -0700

    Create variables.tf

---
+++ sample-novalue/variables.tf
@@ -0,0 +1,7 @@
+# variable "AWS_ACCESS_KEY_ID" {}
+# variable "AWS_SECRET_ACCESS_KEY" {}
+variable "type" {
+  type        = string
+  description = "AWS Region required by Terraform AWS Provider"
+  # default     = "t2.micro"
+  }
