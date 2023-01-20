9369eac0537101d54bb7e49fc87b4b81abbc3209
urischeiner <59268818+urischeiner@users.noreply.github.com>
2023-01-19 17:11:53 -0800

    Update main.tf

--- sample-2/main.tf
+++ sample-2/main.tf
@@ -25,6 +25,7 @@ data "aws_ami" "app_server" {
 resource "aws_instance" "app_server" {
   ami           = "ami-830c94e3"
   instance_type = "t2.small"
+  subnet_id = "subnet-0d4be14d5f95eec6d"
   tags = {
     Name = "ExampleAppServerInstance-Uri-7"
   }
