ab9fc69fa56c55d049c0dfb705cd2ba05817915c
urischeiner <59268818+urischeiner@users.noreply.github.com>
2023-01-30 13:08:25 -0800

    Update output.tf

--- sample-2/output.tf
+++ sample-2/output.tf
@@ -1,14 +1,14 @@
 output "instance_id" {
   description = "ID of the EC2 instance"
-  value       = aws_instance.app_server.id
+  value       = aws_instance.my_ec2.id
 }
 
 output "instance_public_ip" {
   description = "Public IP address of the EC2 instance"
-  value       = aws_instance.app_server.public_ip
+  value       = aws_instance.my_ec2.public_ip
 }
 
 output "cpu_count" {
   description = "Public IP address of the EC2 instance"
-  value       = aws_instance.app_server.cpu_core_count 
+  value       = aws_instance.my_ec2.cpu_core_count 
 }
