a76ddfd772d7c8792d992b8a40d5653b3b9b2c4f
urischeiner <59268818+urischeiner@users.noreply.github.com>
2023-03-06 16:57:37 -0800

    Update outputs.tf

--- Sample-local/outputs.tf
+++ Sample-local/outputs.tf
@@ -1,14 +1,14 @@
 output "instance_public_ip_1" {
   description = "Public IP address of the EC2 instance"
-  value       = aws_instance.app_server[0].public_ip
+  value       = aws_instance.app_server.public_ip
 }
 
 output "instance_public_ip_2" {
   description = "Public IP address of the EC2 instance"
-  value       = aws_instance.app_server[1].public_ip
+  value       = aws_instance.app_server.public_ip
 }
 
 output "instance_public_ip_3" {
   description = "Public IP address of the EC2 instance"
-  value       = aws_instance.app_server[2].public_ip
+  value       = aws_instance.app_server.public_ip
 }
