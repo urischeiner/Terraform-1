ea143ce8ab4734bb22de2ad8de1015529f599513
urischeiner <59268818+urischeiner@users.noreply.github.com>
2023-03-06 16:50:29 -0800

    Create main.tf

---
+++ Sample-local/main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-west-2"
+}
+
+data "aws_ami" "app_server" {
+  filter {
+    name = "image-id"
+    values = ["ami-830c94e3"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.small"
+  subnet_id = "subnet-0d4be14d5f95eec6d"
+  tags = {
+    Name = "ExampleAppServerInstance-Uri-6"
+  }
+  
+  lifecycle {
+    # The AMI ID must refer to an AMI that contains an operating system
+    # for the `x86_64` architecture.
+    precondition {
+      condition     = data.aws_ami.app_server.architecture == "x86_64"
+      error_message = "The selected AMI must be for the x86_64 architecture."
+    }
+  }
+}
