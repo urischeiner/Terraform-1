5262142997f0506ec7c850a31473460b1c894e50
urischeiner <59268818+urischeiner@users.noreply.github.com>
2023-04-05 12:15:11 -0700

    Update output.tf

--- uri-default/output.tf
+++ uri-default/output.tf
@@ -1,11 +1,24 @@
 output "vpc_arn" {
+  description = "VPN ARN"
   value = aws_vpc.main.arn
 }
 
 output "vpc_id" {
+  description = "VPN id"
   value = aws_vpc.main.id
 }
 
 output "subnet_id" {
+    description = "Subnet ARN"
     value = aws_subnet.main.id
+}
+
+output "instance_type" {
+    description = "VM Instance type"
+    value = aws_instance.ec2-be.instance_type
+}
+
+output "ip" {
+    description = "VM private IP"
+    value = aws_instance.ec2-be.private_ip
 }
