6e9ea264b93f8479c0cdf05ced1487be56a7b566
urischeiner <59268818+urischeiner@users.noreply.github.com>
2023-04-05 15:40:33 -0700

    Update main.tf

--- uri-default/main.tf
+++ uri-default/main.tf
@@ -36,9 +36,9 @@ resource "aws_subnet" "main" {
 resource "aws_instance" "ec2-be" {
   instance_type = var.instance_type
   ami           = "ami-830c94e3"
-  subnet_id = aws_subnet.main.id
+#   subnet_id = aws_subnet.main.id
   tags = {
-    name = "CLI-local"
+    name = "ec2-be"
     extra= var.extra
   }
 }
