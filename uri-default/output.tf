e223561d7e6407c3a4448fd04b6ff6c29b45f170
urischeiner <59268818+urischeiner@users.noreply.github.com>
2023-04-06 15:51:08 -0700

    Update output.tf

--- uri-default/output.tf
+++ uri-default/output.tf
@@ -22,3 +22,18 @@ output "ip" {
     description = "VM private IP"
     value = aws_instance.ec2-be.private_ip
 }
+
+output "bucket_arn" {
+  description = "ARN of the bucket"
+  value       = aws_s3_bucket.s3_bucket.arn
+}
+
+output "bucket_name" {
+  description = "Name (id) of the bucket"
+  value       = aws_s3_bucket.s3_bucket.id
+}
+
+output "bucket_domain" {
+  description = "Domain name of the bucket"
+  value       = aws_s3_bucket_website_configuration.s3_bucket.website_domain
+}
