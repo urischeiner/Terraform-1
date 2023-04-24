4125c260c7b74f941eef6ca7431ff3beccd3f417
urischeiner <59268818+urischeiner@users.noreply.github.com>
2023-04-24 14:57:42 +0100

    Update main.tf

--- uri-default2/main.tf
+++ uri-default2/main.tf
@@ -33,7 +33,7 @@ resource "aws_subnet" "main" {
 }
 
 resource "aws_instance" "ec2-be" {
-  instance_type = var.instance_type
+  instance_type = t2.small
   ami           = "ami-830c94e3"
 #   subnet_id = aws_subnet.main.id
   tags = {
