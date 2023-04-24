f614fa712b01ccb67cf7814c3acdca564b9533ac
urischeiner <59268818+urischeiner@users.noreply.github.com>
2023-04-24 14:53:04 +0100

    Create main.tf

---
+++ uri-default2/main.tf
@@ -0,0 +1,45 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+
+provider "aws" {
+  region  = "us-west-2"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_instance" "ec2-be" {
+  instance_type = var.instance_type
+  ami           = "ami-830c94e3"
+#   subnet_id = aws_subnet.main.id
+  tags = {
+    name = "ec2-be"
+  }
+}
+
+
+
