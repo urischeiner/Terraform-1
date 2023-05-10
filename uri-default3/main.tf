3bd6ebdb547ea4af1220df21cca1b5f52ed08be4
urischeiner <59268818+urischeiner@users.noreply.github.com>
2023-05-09 21:48:11 -0700

    Update main.tf

--- uri-default3/main.tf
+++ uri-default3/main.tf
@@ -75,6 +75,14 @@ resource "aws_s3_bucket_public_access_block" "s3_bucket" {
 #   acl = "public-read"
 # }
 
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = aws_s3_bucket.s3_bucket.id
+  rule {
+    acls_disabled = false
+  }
+}
+
+
 resource "aws_s3_bucket_policy" "s3_bucket" {
   bucket = aws_s3_bucket.s3_bucket.id
 
