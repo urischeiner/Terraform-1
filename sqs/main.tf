31d6d83a65f6ed174c12d2e10701d0d187f76a5f
urischeiner <59268818+urischeiner@users.noreply.github.com>
2024-09-26 18:02:41 -0700

    Update main.tf

--- sqs/main.tf
+++ sqs/main.tf
@@ -1,3 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+
+provider "aws" {
+  region  = "us-west-2"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "main"
+  }
+}
+
 module "BBC" {
   source  = "app.harness.io/l7HREAyVTnyfUsfUtPZUow/BBC/sqs"
   version = "v1.0.2"
